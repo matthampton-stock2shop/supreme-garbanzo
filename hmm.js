3fbd569a8408e61064572e7e516625d95573e426
Matt Hampton <matt.hampton@stock2shop.com>
2023-08-28 09:28:11 +0200

    hmm

---
+++ hmm.js
@@ -0,0 +1,9 @@
+
+function addTo(a, b) {
+    a = a + b
+}
+
+let a = 5
+let b = 2
+addTo(a, b)
+console.info("OUT: ", a)
---
+++ hmm.py
@@ -0,0 +1,10 @@
+
+def addTo(a, b):
+    a = a + b
+
+
+if __name__ == '__main__':
+    a = 5
+    b = 2
+    addTo(a, b)
+    print(a)
